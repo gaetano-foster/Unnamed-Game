6a9f0537ab169e5d056983dc714be3f5c5348214
Gaetano Foster <gaetanox109@gmail.com>
2022-06-28 15:44:01 -0400

    Change max fps to 72.

--- src/Launcher.cc
+++ src/Launcher.cc
@@ -1,7 +1,7 @@
 #include "Mineclone.hh"
 
 int main() {
-    Mineclone game("Hello World", 1280, 720, 72);
+    Mineclone game("Hello World", 1280, 720, 1000);
     game.start();
     return game.getExitCode();
 }
